a8bd2ae43b398b724af09314c6f48e9c396479d9
schifzt <26299162+schifzt@users.noreply.github.com>
2021-09-26 12:40:09 +0900

    reset src/

--- src/js/palette.js
+++
@@ -1,8 +0,0 @@
-const blue = {
-	line: 'rgba(0, 0, 255, 1)',
-	fill: 'rgba(120, 120, 255, 0.75)'
-}
-const red = {
-	line: 'rgba(255, 0, 0, 1)',
-	fill: 'rgba(255, 128, 128, 0.75)'
-}
--- src/js/types.js
+++
@@ -1,7 +0,0 @@
-class Points2D {
-	constructor(file) {
-		this.ps = readCSV(file);
-		this.x = this.ps[0].map(x => parseFloat(x));
-		this.y = this.ps[1].map(x => parseFloat(x));
-	}
-};
